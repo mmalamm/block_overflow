e76518509d431a220940ab444bdeca3a7edd3950
M M Alam <alam@mmtutor.com>
2019-05-13 16:31:03 -0400

    start creating observables

--- src/App.js
+++ src/App.js
@@ -13,30 +13,33 @@ export default function App() {
   const [board, setBoard] = useState(createInitState());
   const [keypress, setKeypress] = useState(null);
   useEffect(() => {
-    const boardTick$ = interval(1000)
-    .pipe(
+    const boardTick$ = interval(250).pipe(
       map(i => i % 20),
       map(j => {
         const copy = [...board.map(row => [...row])];
         copy[j][4] = "b";
         return copy;
-      }),
-    )
-    const keypress$ = fromEvent(document, "keydown").subscribe(({ key }) => {
+      })
+    );
+    const keyPress$ = fromEvent(document, "keydown");
 
-    })
+    keyPress$.subscribe(({ key }) => {
+      setKeypress(key);
+    });
 
     const game$ = merge(boardTick$);
-    game$
-      .subscribe(board => setBoard(board));
+    game$.subscribe(board => setBoard(board));
   }, []);
-  
+
   return (
     <div className="App">
       {board.map((row, i) => (
         <div style={{ display: "flex" }} key={i}>
           {row.map((cell, j) => (
-            <div style={{ color: cell === "b" ? "blue" : "#222", padding: '3px' }} key={j}>
+            <div
+              style={{ color: cell === "b" ? "blue" : "#222", padding: "3px" }}
+              key={j}
+            >
               {cell}
             </div>
           ))}
