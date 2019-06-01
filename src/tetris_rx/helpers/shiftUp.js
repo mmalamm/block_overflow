9ae68ac9b512fd0db84d7c51d2bd27d81eff9276
M M Alam <alam@mmtutor.com>
2019-06-01 05:24:01 -0400

    implement shift up

--- src/tetris_rx/helpers/shiftUp.js
+++ src/tetris_rx/helpers/shiftUp.js
@@ -1,14 +1,37 @@
-const shiftUp = ({
-  board,
-  playerPiece: { pieceName, x, y, orientation, offset },
-  upcomingPieces,
-  score
-}) => {
+import willCollide from "./willCollide";
+import createNewBoard from "./createNewBoard";
+const bonusMap = [0, 10, 25, 45, 70];
+const shiftUp = state => {
+  const { board, playerPiece: pce, upcomingPieces, score } = state;
+  const { y } = pce;
+  let tempY = y + 1;
+  while (!willCollide(board, { ...pce, y: tempY })) {
+    tempY++;
+  }
+
+  const newUpcomingPieces = upcomingPieces.slice();
+  const nextPieceName = newUpcomingPieces.pop();
+  const tickedBoard = createNewBoard(board, { ...pce, y: tempY });
+  const clearedBoard = tickedBoard.filter(row => row.includes("e"));
+  const numRowsCleared = 20 - clearedBoard.length;
+  const newBoard = [
+    ...[...Array(numRowsCleared)].map(_ => "e".repeat(10)),
+    ...clearedBoard
+  ];
+  const newScore = score + bonusMap[numRowsCleared];
+
   return {
-    board,
-    playerPiece: { pieceName, x, y, orientation, offset },
-    upcomingPieces,
-    score
+    ...state,
+    playerPiece: {
+      pieceName: nextPieceName,
+      x: 4,
+      y: 0,
+      orientation: 0,
+      offset: 0
+    },
+    board: newBoard,
+    upcomingPieces: newUpcomingPieces,
+    score: newScore
   };
 };
 
